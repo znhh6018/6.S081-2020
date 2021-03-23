88f57b98868514ff3b6fb8587401442703d22617
znhh6018 <389565786@qq.com>
2021-03-23 21:44:19 +0800

    modify relevent variables

--- kernel/fs.c
+++ kernel/fs.c
@@ -427,35 +427,35 @@ bmap(struct inode *ip, uint bn)
 
 // Return the disk block address of the nth block in inode ip.
 // If there is no such block, bmap allocates one.
-static uint
-bmap_original(struct inode *ip, uint bn)
-{
-  uint addr, *a;
-  struct buf *bp;
-
-  if(bn < NDIRECT){
-    if((addr = ip->addrs[bn]) == 0)
-      ip->addrs[bn] = addr = balloc(ip->dev);
-    return addr;
-  }
-  bn -= NDIRECT;
-
-  if(bn < NINDIRECT){
-    // Load indirect block, allocating if necessary.
-    if((addr = ip->addrs[NDIRECT]) == 0)
-      ip->addrs[NDIRECT] = addr = balloc(ip->dev);
-    bp = bread(ip->dev, addr);
-    a = (uint*)bp->data;
-    if((addr = a[bn]) == 0){
-      a[bn] = addr = balloc(ip->dev);
-      log_write(bp);
-    }
-    brelse(bp);
-    return addr;
-  }
-
-  panic("bmap: out of range");
-}
+//static uint
+//bmap_original(struct inode *ip, uint bn)
+//{
+//  uint addr, *a;
+//  struct buf *bp;
+//
+//  if(bn < NDIRECT){
+//    if((addr = ip->addrs[bn]) == 0)
+//      ip->addrs[bn] = addr = balloc(ip->dev);
+//    return addr;
+//  }
+//  bn -= NDIRECT;
+//
+//  if(bn < NINDIRECT){
+//    // Load indirect block, allocating if necessary.
+//    if((addr = ip->addrs[NDIRECT]) == 0)
+//      ip->addrs[NDIRECT] = addr = balloc(ip->dev);
+//    bp = bread(ip->dev, addr);
+//    a = (uint*)bp->data;
+//    if((addr = a[bn]) == 0){
+//      a[bn] = addr = balloc(ip->dev);
+//      log_write(bp);
+//    }
+//    brelse(bp);
+//    return addr;
+//  }
+//
+//  panic("bmap: out of range");
+//}
 
 // Truncate inode (discard contents).
 // Caller must hold ip->lock.
